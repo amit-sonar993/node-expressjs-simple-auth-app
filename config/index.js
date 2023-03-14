9b77a476380324ae426c1567276e78f8ee588c74
Amit Sonar <amit.sonar993@gmail.com>
2023-03-14 13:02:04 +0545

    dotenv package added and all env variables are organized in config file

---
+++ config/index.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+
+module.exports = {
+    endpoint: process.env.API_URL,
+    masterKey: process.env.API_KEY,
+    port: normalizePort(process.env.PORT || '3000')
+};
